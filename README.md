15ccb272f32e11a758dc02f2db7c584181205988
Ron Waldon <jokeyrhyme@gmail.com>
2014-03-08 23:34:16 +1100

    initial commit with proof of concept code

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-noscript.js
-===========
+# noscript.js
 
 stop other JavaScript (as best as can be done) and show `noscript` elements
+
+
+## Usage
+
+Include `noscript.js` on your web page. This installs a global `noscript`
+object. You may want the poly-fills in the `lib` directory if you plan on using
+this with older browsers.
+
+**TODO**: provide a concatenated version so that there is just one file
+
+### noscript.show()
+
+This will find all `noscript` elements on the page, and make their contents
+visible, just as though the browser had JavaScript disabled.
+
+**Caution**: this will only work on elements that were on the page _before_
+`noscript.show()` was called. That means that you'll likely want to do this
+after all other HTML elements have been declared (e.g. at the bottom of the
+`body` element.
+
+### noscript.lockdown()
+
+There doesn't seem to be a way to disable JavaScript via JavaScript. So, this
+method hunts down various APIs that govern side-effects (e.g. HTML DOM, AJAX,
+etc) and tampers with them to the point where they hopefully become useless.
+
+**Caution**: you need to call this _after_ calling `noscript.show()`, because
+that method will not work at all after `.lockdown()`. If you have no
+`noscript` elements on the page, then feel free to call this as early as
+possible. You'll want to call this _before_ any JavaScript to be blocked.
+
+
+## Example
+
+```
+<!DOCTYPE html>
+<html>
+  <head>...</head>
+  <body>
+    <noscript>Error: bad device or web browser</noscript>
+    ...
+    <script src="noscript.js"></script>
+    <script>
+    if (!window.JSON) {
+      noscript.show();
+      noscript.lockdown();
+    }
+    </script>
+  </body>
+</html>
+```
---
+++ noscript.js
@@ -0,0 +1,92 @@
+/*jslint indent:2, browser:true*/
+
+/*global HTMLDocument, HTMLElement, Element, Node*/
+
+var noscript = (function (window) {
+  'use strict';
+
+  var noscripts, trashees, t;
+
+  function unwrapElements(elements) {
+    var i, j, el, child, text;
+    elements = document.getElementsByTagName('noscript');
+    i = elements.length;
+    while (i > 0) {
+      i -= 1;
+      el = elements[i];
+      j = el.childNodes.length;
+      while (j > 0) {
+        j -= 1;
+        child = el.childNodes[j];
+        // note: textContent / innerText may trim the contents
+        text = child.textContent || child.innerText;
+        el.insertAdjacentHTML('beforebegin', text);
+      }
+      el.parentNode.removeChild(el);
+    }
+  }
+
+  function noop() {
+    throw new Error('synthetic error is used to disable JavaScript');
+  }
+
+  function trashObject(obj) {
+    var prop;
+    if (obj && typeof obj === 'object') {
+      for (prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+          if ((obj === window || obj === document) &&
+              trashObject.AVOID_PROPS.indexOf(prop) > -1) {
+            continue;
+          }
+          if (trashObject.AVOID_TYPES.indexOf(typeof obj[prop]) > -1) {
+            continue;
+          }
+          try {
+            obj[prop] = null;
+            delete obj[prop];
+          } catch (err1) {}
+          try {
+            if (obj[prop]) {
+              obj[prop] = noop;
+            }
+          } catch (err2) {}
+        }
+      }
+    }
+  }
+  trashObject.AVOID_TYPES = ['number', 'string', 'boolean', 'array'];
+  trashObject.AVOID_PROPS = ['location', 'frameElement'];
+
+  // clumsily implement a blacklist of JavaScript objects
+
+  trashees = [
+    window.console || window.debug,
+    window,
+    HTMLDocument && HTMLDocument.prototype,
+    HTMLElement && HTMLElement.prototype,
+    Element && Element.prototype,
+    Node && Node.prototype,
+    XMLHttpRequest && XMLHttpRequest.prototype
+  ];
+
+  return {
+    show: function () {
+      // unwrap all noscript elements
+      // noscript tags shouldn't be nested, but we'll cover this case anyway
+      noscripts = document.getElementsByTagName('noscript');
+      while (noscripts.length) {
+        unwrapElements(noscripts);
+        noscripts = document.getElementsByTagName('noscript');
+      }
+    },
+    lockdown: function () {
+      t = trashees.length;
+      while (t > 0) {
+        t -= 1;
+        trashObject(trashees[t]);
+      }
+    }
+  };
+
+}(this));
